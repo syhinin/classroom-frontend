e7287e2b90bd7b0bbb7eec3759ff0ac76bb7db6b
Oleksandr <syhinin@gmail.com>
2026-02-17 21:26:54 +0100

    feat: Introduce a custom Dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import {
   AuthProvider,
-  GitHubBanner,
   Refine,
-  WelcomePage,
 } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-
+import { Home } from "lucide-react";
 import routerProvider, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router";
 import axios from "axios";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+
 import "./App.css";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
@@ -20,6 +19,8 @@ import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { CredentialResponse } from "./interfaces/google";
 import { dataProvider } from "./providers/data";
 import { parseJwt } from "./utils/parse-jwt";
+import { Dashboard } from "./pages/Dashboard";
+import { Layout } from "./components/refine-ui/layout/layout";
 
 const axiosInstance = axios.create();
 axiosInstance.interceptors.request.use((config) => {
@@ -110,7 +111,6 @@ function App() {
 
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ThemeProvider>
           <DevtoolsProvider>
@@ -124,9 +124,25 @@ function App() {
                 warnWhenUnsavedChanges: true,
                 projectId: "kZnXom-0PZWNj-DOtO7k",
               }}
+              resources={[
+                {
+                  name: "dashboard",
+                  list: "/",
+                  meta: {
+                    label: "Home",
+                    icon: <Home />,
+                  },
+                },
+              ]}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+                <Route element={
+                  <Layout>
+                    <Outlet />
+                  </Layout>
+                }>
+                </Route>
+                <Route path="/" element={<Dashboard />} />
               </Routes>
               <Toaster />
               <RefineKbar />
